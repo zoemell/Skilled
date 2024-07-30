ba1145c6e2a2e3c197e5cead3efeffa8af8f4978
Zoe Meller <zoeyaelmeller@gmail.com>
2024-07-30 13:01:04 -0400

    Initial Commit

---
+++ Skilled/SkilledApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkilledApp.swift
+//  Skilled
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SkilledApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
