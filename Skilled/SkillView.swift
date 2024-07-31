f3962300cafcfb1efc7c863ba5b634aed453531b
Zoe Meller <zoeyaelmeller@gmail.com>
2024-07-31 15:38:40 -0400

    finished the video background, home page, working on zigzag stack

---
+++ Skilled/SkillView.swift
@@ -0,0 +1,18 @@
+//
+//  SkillView.swift
+//  Skilled
+//
+//  Created by Scholar on 7/31/24.
+//
+
+import SwiftUI
+
+struct SkillView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SkillView()
+}
---
+++ Skilled/lazyVGrid.swift
@@ -0,0 +1,18 @@
+//
+//  SkillPageGuitar.swift
+//  Skilled
+//
+//  Created by Scholar on 7/31/24.
+//
+
+import SwiftUI
+
+struct SkillPageGuitar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SkillPageGuitar()
+}
